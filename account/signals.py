b48df73349f96773c73c3a90c7bc898715e43252
Christian Dietrich <stettberger@dokucode.de>
2020-06-08 21:06:15 +0200

    remove users from kirby if they are no longer in the genossenschaft

--- account/signals.py
+++ account/signals.py
@@ -7,9 +7,6 @@ import os
 import logging
 logger = logging.getLogger(__name__)
 
-user_changed = django.dispatch.Signal(providing_args=["username"])
-
-
 KIRBY_TEMPLATE = """<?php
 return [
     'id' => '{username}',
@@ -18,21 +15,28 @@ return [
     'language' => 'de',
     'role' => 'LdapUser'
 ];
+
 """
 
+user_changed = django.dispatch.Signal(providing_args=["username"])
 @django.dispatch.receiver(user_changed)
 def user_changed_hook(sender, **kwargs):
     username = kwargs['username']
     logger.info("User Changed Hook: %s", username)
     user = LDAPBackend().populate_user(username)
-    groups = user.ldap_user.group_names
+    groups = set(user.ldap_user.group_names)
 
-    if 'genossenschaft' in groups or 'wettbewerb-jury' in groups:
-        directory = Path(settings.KIRBY_ACCOUNTS_DIRECTORY) / username
+    ################################################################
+    # Kirby
+    directory = Path(settings.KIRBY_ACCOUNTS_DIRECTORY) / username
+    index_php = directory / "index.php"
+    if groups & set(['genossenschaft',
+                     'wettbewerb-jury',
+                     'amsel-kollektiv',
+                     'vorstand']):
         if not os.path.exists(directory):
             os.mkdir(directory)
 
-        index_php = directory / "index.php"
         index_php_content = KIRBY_TEMPLATE.format(
             username=user.username,
             email=user.email
@@ -41,3 +45,9 @@ def user_changed_hook(sender, **kwargs):
             with open(index_php, "w+") as fd:
                 fd.write(index_php_content)
             logger.info("Created/Updated Kirby Login for %s", user.username)
+    else:
+        if directory.exists():
+            if index_php.exists():
+                index_php.unlink()
+            directory.rmdir()
+            logger.info("Deleted Kirby Login for %s", user.username)
