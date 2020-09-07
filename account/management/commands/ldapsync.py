353701e50958088e2c80807a8df40377dce453f2
Christian Dietrich <stettberger@dokucode.de>
2020-09-07 08:41:34 +0200

    account: rename kirby

--- account/management/commands/ldapsync.py
+++ account/management/commands/ldapsync.py
@@ -13,7 +13,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
-    help = 'Update Kirby Accounts'
+    help = 'Sync Account/Group Information with outside world'
     def add_arguments(self, parser):
         parser.add_argument("--user", '-u', default=None,
                             help="Update only User")
